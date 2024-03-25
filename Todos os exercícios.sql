9db4ba88ab88bdf2eb3f104a8e91d454f3d09c24
Ticiano <126998734+ticianooliveira@users.noreply.github.com>
2024-03-24 23:47:46 -0300

    Update Todos os exercícios.sql

--- Todos os exercícios.sql
+++ Todos os exercícios.sql
@@ -102,43 +102,128 @@ em sala de aula.
 // Escreva uma consulta para listar todos os produtos na categoria 'Eletrônicos' com seus
 respectivos valores.
 
+SELECT nome_produto, valor_produto
+FROM produtos
+WHERE categoria_produto = 'Eletronicos';
 
 
 
 // Calcule o total de vendas (valor_compra) para cada usuário e ordene os resultados em
 ordem decrescente de valor total.
 
+SELECT
+    cod_usuario,
+    SUM(valor_compra) AS total_vendas
+FROM
+    vendas
+GROUP BY
+    cod_usuario
+ORDER BY
+    total_vendas DESC;
 
 
 
 // Obtenha uma lista de usuários que fizeram compras com cartão de crédito
 ('forma_pagamento' é igual a 'Cartão de Crédito').
 
+SELECT DISTINCT
+    u.cod_usuario,
+    u.data_cadastro,
+    u.faixa_etaria,
+    u.cidade,
+    u.estado
+FROM
+    usuarios u
+INNER JOIN
+    vendas v ON u.cod_usuario = v.cod_usuario
+WHERE
+    v.forma_pagamento = 'cartao_credito';
 
 
-
-// Calcule a média de valor de compra por produto na categoria 'Roupas' e liste os 
+// Calcule a média de valor de compra por produto na categoria 'Moda e beleza' e liste os 
   produtos com valores acima dessa média.
 
+SELECT 
+    p.cod_produto,
+    p.nome_produto,
+    p.valor_produto
+FROM 
+    produtos p
+WHERE 
+    p.categoria_produto = 'Moda e beleza'
+    AND p.valor_produto > (
+        SELECT 
+            AVG(p.valor_produto) AS media_valor_compra
+        FROM 
+            produtos p
+        WHERE 
+            p.categoria_produto = 'Moda e beleza'
+    );
 
 
 
 // Criar uma view materializada que mostra a quantidade total vendida de cada produto.
   
+CREATE MATERIALIZED VIEW quantidade_total_vendida_por_produto AS
+SELECT 
+    cod_produto,
+    SUM(quantidade) AS quantidade_total_vendida
+FROM 
+    vendas
+GROUP BY 
+    cod_produto;
 
 
-
-// Qual é a quantidade total vendida do produto "Smartphone"?
+// Qual é a quantidade total vendida do produto "Smartphone Samsung"?
   
+
+SELECT 
+    SUM(quantidade) AS quantidade_total_vendida
+FROM 
+    vendas
+WHERE 
+    cod_produto = (SELECT cod_produto FROM produtos WHERE nome_produto = 'Smartphone Samsung');
+
 
 
 
 // Criar uma view virtual que mostra os nomes dos produtos com valor superior à média dos produtos da mesma categoria.
   
+CREATE VIEW produtos_acima_media_categoria AS
+SELECT 
+    p.nome_produto,
+    p.valor_produto,
+    p.categoria_produto
+FROM 
+    produtos p
+WHERE 
+    p.valor_produto > (
+        SELECT 
+            AVG(p2.valor_produto) 
+        FROM 
+            produtos p2 
+        WHERE 
+            p2.categoria_produto = p.categoria_produto
+    );
 
 
 
 // Quais são os nomes dos produtos da categoria "Eletrônicos" com valor superior à média?
+
+SELECT 
+    nome_produto
+FROM 
+    produtos
+WHERE 
+    categoria_produto = 'Eletrônicos'
+    AND valor_produto > (
+        SELECT 
+            AVG(valor_produto) 
+        FROM 
+            produtos 
+        WHERE 
+            categoria_produto = 'Eletrônicos'
+    );
 
 
 
@@ -146,9 +231,33 @@ ordem decrescente de valor total.
 // Criar uma view que apresente o nome do produto, a quantidade vendida e o valor total
 da venda para cada venda realizada.
 
+CREATE VIEW VendasDetalhadas AS
+SELECT
+    p.nome_produto,
+    v.quantidade,
+    v.valor_compra AS valor_total_venda
+FROM
+    vendas v
+JOIN
+    produtos p ON v.cod_produto = p.cod_produto;
 
 
 
 // Criar uma view que apresente a cidade com o maior número de vendas em 2023, com
 a quantidade total de vendas e o valor total das vendas.
 
+/*CREATE VIEW MaiorNumeroVendas2023 AS
+SELECT
+    cidade,
+    COUNT(*) AS quantidade_total_vendas,
+    SUM(valor_compra) AS valor_total_vendas
+FROM
+    vendas
+WHERE
+    EXTRACT(YEAR FROM data_compra) = 2023
+GROUP BY
+    cidade
+ORDER BY
+    COUNT(*) DESC
+LIMIT 1;
+*/
